1dd7592ae1ec709962a635b75b7c751e9e4889cf
Yahya Bakthiarnia <bakhtiarniayahya1378@gmail.com>
2023-10-03 14:45:11 +0330

    - Add local DB - Add some relation tables

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -6,4 +6,6 @@ public class Pokemon
     public string Name { get; set; }
     public DateTime Birthdate { get; set; }
     public ICollection<Review> Reviews { get; set; }
+    public ICollection<PokemonOwner> PokemonOwners { get; set; }
+    public ICollection<PokemonCategory> PokemonCategories { get; set; }
 }
